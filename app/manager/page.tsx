08c3c32a48f80484c86a6eb00a4a5567d6263f46
Balroog1 <devabhi456@gmail.com>
2026-05-17 13:39:55 +0530

    Added logout functionality for manager dashboard

--- app/manager/page.tsx
+++ app/manager/page.tsx
@@ -1,8 +1,43 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function ManagerPage() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+
+    const role = localStorage.getItem("role");
+
+    if (role !== "manager") {
+
+      router.push("/login");
+
+    }
+
+  }, [router]);
 
   return (
 
     <main className="min-h-screen bg-black text-white p-10">
+        <div className="flex justify-end mb-10">
+
+          <button
+            onClick={() => {
+
+            localStorage.removeItem("role");
+
+            router.push("/login");
+
+            }}
+            className="bg-red-500 hover:bg-red-600 transition px-5 py-3 rounded-xl font-semibold"
+          >
+           Logout
+          </button>
+
+        </div>
 
       <h1 className="text-5xl font-bold mb-4">
         Manager Dashboard
